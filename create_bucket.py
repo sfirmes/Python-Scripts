c5ab471cf1b13a0e3c5bc39590af47b0fbaf4284
sfirmes <steve@stevefirmes.com>
2023-02-02 20:52:47 -0500

    Old version of create_bucket.py

--- create_bucket.py
+++ create_bucket.py
@@ -2,25 +2,24 @@ import boto3
 import sys
 import getopt
 
-# Parse the parameters entered and return the variable needed to create the bucket
-def get_parms(argv):
+def create_bucket(argv):
     arg_bucket = ""
     arg_profile = ""
     arg_endpoint = ""
-    arg_secured = ""
-    arg_help = "{0} -b bucket <bucket> -p <profile> -e <endpoint> -s <security>".format(argv[0])
+    arg_ssl = "y"
+    arg_immutable = "y"
+    arg_help = "{0} -b bucket <bucket> -p <profile> -e <endpoint> -s <ssl>".format(argv[0])
     
     try:
-        opts, args = getopt.getopt(argv[1:], "hb:p:e:s:", ["help", "bucket=", "profile=", 
-        "endpoint=", "secured="])
+        opts, args = getopt.getopt(argv[1:], "hb:p:e:s:i:", ["help", "bucket", "profile", 
+        "endpoint", "ssl", "immutable"])
     except:
         print(arg_help)
         sys.exit(2)
     
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-# print the help message
-            print(arg_help)  
+            print(arg_help)  # print the help message
             sys.exit(2)
         elif opt in ("-b", "--bucket"):
             arg_bucket = arg
@@ -28,31 +27,32 @@ def get_parms(argv):
             arg_profile = arg
         elif opt in ("-e", "--endpoint"):
             arg_endpoint = arg
-        elif opt in ("-s", "--security"):
-            arg_secured = arg
+        elif opt in ("-s", "--ssl"):
+            arg_ssl = arg
+        elif opt in ("-i", "--immutable"):
+            arg_immutable = arg
 
-    return [arg_bucket, arg_profile, arg_endpoint, arg_secured]
-
-# Capture the parameters entered using the get_parms function
-if __name__ == "__main__":
-    arg_bucket, arg_profile, arg_endpoint, arg_secured = get_parms(sys.argv)
-
-    
-# Connect to s3 endpoint using the profile from .aws/credentials file
     print('bucket:', arg_bucket)
     print('profile:', arg_profile)
     print('endpoint:', arg_endpoint)
-    print('secured:', arg_secured)
+    print('ssl:', arg_ssl)
+    print('immutable:', arg_immutable)
 
+# Connect to s3 endpoint using the profile from .aws/credentials file
     session = boto3.Session(profile_name=arg_profile)
     credentials = session.get_credentials()
     aws_access_key_id = credentials.access_key
     aws_secret_access_key = credentials.secret_key
 
-    if arg_secured == "y" or arg_secured == "Y":
+    if arg_ssl == "y" or arg_ssl == "Y":
         secure_flag=True
     else:
         secure_flag=False
+
+    if arg_immutable == "y" or arg_ssl == "Y":
+        object_lock=True
+    else:
+        object_lock=False
 
     s3 = boto3.client('s3',
         endpoint_url=arg_endpoint,
@@ -63,4 +63,7 @@ if __name__ == "__main__":
 
     bucket_name = arg_bucket
 
-    s3.create_bucket(Bucket=bucket_name, ObjectLockEnabledForBucket=True)
+    s3.create_bucket(Bucket=bucket_name, ObjectLockEnabledForBucket=object_lock)
+
+if __name__ == "__main__":
+    create_bucket(sys.argv)
